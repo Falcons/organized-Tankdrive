7d5cab49bc31076cba5c844db21ca31a588785d9
madness <78887319+madness-613@users.noreply.github.com>
2025-12-02 20:36:21 -0500

    fixed Constants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public class Constants {
+
+    public static final class DriveConstants {
+        public static final int frontLeftCANID = 4;
+        public static final int frontRightCANID = 3;
+        public static final int backLeftCANID = 2;
+        public static final int backRightCANID = 1;
+    }
+}
